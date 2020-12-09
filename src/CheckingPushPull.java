96b95bfe04de66b473664b82867ad504f8f1696e
prachichourasia <75191304+prachichourasia@users.noreply.github.com>
2020-12-09 09:18:26 +0530

    Update CheckingPushPull.java

--- src/CheckingPushPull.java
+++ src/CheckingPushPull.java
@@ -4,6 +4,7 @@ public class CheckingPushPull {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("checking push commomd");
+		System.out.println("checking pull commomd");
 	}
 
 }
